b28225345398379f3a95beb2b40e48b680238ae3
Tobias Kohn <webmaster@tobiaskohn.ch>
2018-06-08 16:47:01 +0100

    Added graph display to the example

--- run_example.py
+++ run_example.py
@@ -2,3 +2,6 @@ from pyppl import compile_model_from_file
 
 model = compile_model_from_file("examples/if_model_1.py")
 print(model)
+
+# This works only if the necessary modules are installed, i.e. networkx, matplotlib.pyplot
+model.display_graph()
